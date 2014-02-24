38d35b00cdfcad32bd107e35f55ce8669e38cd37
blackdotsh <blackice.sh@gmail.com>
2014-02-24 03:31:23 -0600

    added bash file and readme

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 traceroute-BGP
 ==============
 
-Shows AS Info on top of the standard traceroute
+A bash script that shows AS Info on top of the standard traceroute <br>
+
+It should be simple enough to figure out how to use.
---
+++ trBGP.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+# Licensed under GPLv3
+# created by "black" on LET
+# please give credit if you plan on using this for your own projects
+# https://github.com/blackdotsh
+# Depends on http://www.team-cymru.org/Services/ip-to-asn.html#whois for ASN -> Whois
+
+if [[ -z "$1" ]]
+then
+	echo "IP argurement required. Run the program as: $0 IP_Here";
+	exit 1;
+fi
+
+OIFS=$IFS;
+IFS=$'\n';
+for line in $( traceroute  -A "$1" )
+do
+	echo "$line" | grep "AS" -q;
+	if [ $? -eq 0 ]
+	then
+		AS=$( echo $line | grep -E "\[AS.+?\] " -o | tr -d  "[]" | cut -d " " -f1 );
+		ASResult=$( whois -h whois.cymru.com " -v $AS" | tail -n 1 | cut -d "|" -f 5 );
+		echo "$line" | sed "s/\]/ \-\>$ASResult\]/1";
+ 
+	else
+		echo "$line";
+	fi	
+
+done
+IFS=$OIFS;
+
